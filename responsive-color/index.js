26b346cff0dab4c308cf3c89737a87a6fd8ee799
pguillie <pguillie@student.42.fr>
2019-08-26 18:59:19 +0200

    [change] responsive-color done

--- responsive-color/index.js
+++ responsive-color/index.js
@@ -1,32 +1,14 @@
 var app = require('express')();
 const fs = require('fs');
-const verge = require('verge');
+
+var config;
 
 fs.readFile('./config.json', (err, data) => {
     if (err) throw err;
     config = JSON.parse(data);
-    console.log('type: ' + typeof config + '\nvalue: ' + config[0][2]);
 });
 
 app.get('/', function(req, res) {
-
-    res.send("OK");
-}).listen('8080');
-
-
-/*
-
-[
-  [375, 812, "blue"],
-  [1024, 768, "#ccc"]
-]
-
------------------------
-
-375 * 812 -> blue
-
-414 * 736 -> #ccc
-
-1365 * 978 -> none
-
-*/
+    let str = JSON.stringify(config);
+    res.render('bg.ejs', {jsConfig: str});
+}).listen('8000');
